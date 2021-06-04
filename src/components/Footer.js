a08171e585a704b2c4d2150aa5d41c24fa6bc232
Saibal Kumar Ghosh <saibal.ghosh.1980@gmail.com>
2021-06-04 02:41:34 +0100

    NA

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class Footer extends Component {
     render() {
         return (
-            <div className="bg-success">
+            <div className="bg-light">
                 Footer.....
             </div>
         )
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
 
 export default class Header extends Component {
-    render() {
-        return (
-            <div className="bg-danger" align="left">
-                Header......
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="bg-danger" align="left">
+        <Navbar bg="light" variant="light">
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav variant="pills" defaultActiveKey="/home">
+            <Nav.Item>
+              <Nav.Link href="/home">Active</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="link-1">Option 2</Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="disabled" disabled>
+                Disabled
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </Navbar>
+      </div>
+    );
+  }
 }
